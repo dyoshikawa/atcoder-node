774bedcdcb60af2e7b04e3d61cec27193011524c
yoshikawada@crunchtimer.jp <yoshikawada@crunchtimer.jp>
2019-05-18 08:37:11 +0900

    fix: abc122_c

--- abc122_c/main.ts
+++ abc122_c/main.ts
@@ -23,19 +23,3 @@ export const main = (
   })
   return res
 }
-
-const input: string = require('fs').readFileSync('/dev/stdin', 'UTF-8')
-const lines = input.split('\n')
-
-const NQ = lines[0].split(' ')
-const N = Number(NQ[0])
-const Q = Number(NQ[1])
-const S = lines[1]
-
-const lrList: Array<Array<number>> = []
-for (let i = 0; i < Q; i++) {
-  lrList.push(lines[2 + i].split(' ').map(l => Number(l)))
-}
-
-const res = main(N, Q, S, lrList)
-res.forEach(r => console.log(r))
